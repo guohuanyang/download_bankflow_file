1e665c0b3b3e28364a81ee72441c7fc64c90eaf4
guohuanyang <244651200@qq.com>
2021-08-02 17:57:59 +0800

    add try except

--- utils.py
+++ utils.py
@@ -12,15 +12,19 @@ import os
 
 
 def download_pdf(url, dir_name='./files', new_pdf_name='test.pdf'):
-    response_info = requests.get(url, stream=True)
-    response_info.encoding = response_info.apparent_encoding
-    if not os.path.exists(dir_name):
-        os.mkdir(dir_name)
-    pdf_path = os.path.join(dir_name, new_pdf_name)
-    with open(pdf_path, "wb") as f:
-        for chunk in response_info.iter_content(chunk_size=1024):
-            if chunk:
-                f.write(chunk)
+    print(url)
+    try:
+        response_info = requests.get(url, stream=True)
+        response_info.encoding = response_info.apparent_encoding
+        if not os.path.exists(dir_name):
+            os.mkdir(dir_name)
+        pdf_path = os.path.join(dir_name, new_pdf_name)
+        with open(pdf_path, "wb") as f:
+            for chunk in response_info.iter_content(chunk_size=1024):
+                if chunk:
+                    f.write(chunk)
+    except Exception as e:
+        print(e)
 
 
 def split_txt(filepath='file_info.txt'):
@@ -29,4 +33,3 @@ def split_txt(filepath='file_info.txt'):
     lines = list(map(lambda x: x.split(r"	"), lines))
     lines = [[file_id, file_name, unique_name.replace("\n", ""), y] for file_id, file_name, unique_name, *y in lines]
     return lines
-
